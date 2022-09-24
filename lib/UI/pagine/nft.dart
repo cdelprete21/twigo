fe224811bc194f68a990ef8acc14e25d02ccbf60
cdelprete21 <cdelprete21@gmail.com>
2022-09-24 20:29:48 +0200

    prova nft

--- lib/UI/pagine/nft.dart
+++ lib/UI/pagine/nft.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 
 class nft extends StatelessWidget {
-  final List<String> userPosts = [];
+  final List<String> data = [
+    'one',
+    'two',
+    'three',
+  ];
+
+  final List imglst = [
+    Image.asset('assets/cd.png'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 3,
-      gridDelegate: 
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(2.0),
-          child: Container(
-            color: Colors.green[100],
-          ),
-        );
-      }
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Colors.orange,
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: ListTile(
+                title: Text(data[index]),
+                leading: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/cd.png'),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
