04daa5059eeedf3375476459bdf731082c42b157
cdelprete21 <cdelprete21@gmail.com>
2022-09-27 18:48:19 +0200

    settings not

--- lib/UI/pagine/Impostazioni.dart
+++ lib/UI/pagine/Impostazioni.dart
@@ -13,60 +13,104 @@ class _ImpostazioniState extends State<Impostazioni> {
     return Container(
       color: Colors.black,
       padding: EdgeInsets.only(left: 16, top: 25, right: 16),
-      child: ListView(
-        children: [
-          Row(
-            children: [
-              Icon(
-                Icons.person,
-                color: Colors.orange,
-              ),
-              SizedBox(width: 8,),
-              Text(
-                "Account",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  color: Colors.orange,
                 ),
-              ),
-            ],
-          ),
-          Divider(
-            color: Colors.grey[900],
-            height: 15,
-            thickness: 2,
-          ),
-          GestureDetector(
-            onTap: (){
-              showDialog(
-                context: context, builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: Text("Cambia password"),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text("Option 1"),
-                        Text("Option 2"),
-                        Text("Option 3"),
-                      ],
-                    ),
-                    actions: [
-                      FlatButton(
-                        onPressed: (){
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Close"),
-                      ),
+                SizedBox(width: 8,),
+                Text(
+                  "Account",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.grey[900],
+              height: 15,
+              thickness: 2,
+            ),
+            buildAccountOptionRow(context, "Email"),
+            buildAccountOptionRow(context, "Nome"),
+            buildAccountOptionRow(context, "Password"),
+            buildAccountOptionRow(context, "Privacy & Security"),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.volume_up_outlined,
+                  color: Colors.orange,
+                ),
+                SizedBox(width: 8,),
+                Text(
+                  "Notifications",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Divider(
+              color: Colors.grey[900],
+              height: 15,
+              thickness: 2,
+            ),
+            buildAccountOptionRow(context, "Email"),
+            buildAccountOptionRow(context, "Nome"),
+            buildAccountOptionRow(context, "Password"),
+            buildAccountOptionRow(context, "Privacy & Security"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector buildAccountOptionRow(BuildContext context, String title) {
+    return GestureDetector(
+          onTap: (){
+            showDialog(
+              context: context, builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text(title),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Option 1"),
+                      Text("Option 2"),
+                      Text("Option 3"),
                     ],
-                  );
-                });
-            },
+                  ),
+                  actions: [
+                    FlatButton(
+                      onPressed: (){
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Close"),
+                    ),
+                  ],
+                );
+              });
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "Cambia password",
+                  title,
                   style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w500,
@@ -80,8 +124,6 @@ class _ImpostazioniState extends State<Impostazioni> {
               ],
             ),
           ),
-        ],
-      ),
-    );
+        );
   }
 }
--- lib/UI/pagine/merch.dart
+++ lib/UI/pagine/merch.dart
@@ -5,13 +5,13 @@ class merch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 3,
-      //ListDelegate: 
-         // SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+    return GridView.builder(
+      itemCount: 4,
+      gridDelegate: 
+         SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
       itemBuilder: (context, index) {
         return Padding(
-          padding: const EdgeInsets.fromLTRB(10, 5, 10, 50),
+          padding: const EdgeInsets.all(2.0),
           child: Align(
             heightFactor: 0.7,
             alignment: Alignment.topLeft,
